94e424722652604c4fd8d6b883974b066123165f
mjrdev <jrgomesdev@gmail.com>
2022-09-12 00:27:34 -0300

    tasks app in ReactJs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Task from './components/Task'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+
+        <div className='controlls'>
+          <h1>Minhas Tarefas Diárias</h1>
+          <input type="text" placeholder="O que temos para hoje?"></input>
+          <button>add</button>
+        </div>
+
+        <div className='tasks'>
+          <Task />
+          <Task />
+          <Task />
+          <Task />
+          <Task />
+          <Task />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Task.js
@@ -0,0 +1,11 @@
+function Task() {
+  return (
+    <div className="Task">
+      <h1>Minha Tarefas 1</h1>
+      <button>remove</button>
+      <button>edit</button>
+    </div>
+  );
+}
+
+export default Task;
